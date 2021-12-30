8ed95b4a7ae9b79278c3b1092f9edd2cd0b16389
Ronak Jeengar <ronakjeengar7860@gmail.com>
2021-12-31 03:27:06 +0530

    Efficient way

--- src/com/Ronak/Arrays/SwapAlternate.java
+++ src/com/Ronak/Arrays/SwapAlternate.java
@@ -3,15 +3,9 @@ package com.Ronak.Arrays;
 public class SwapAlternate {
 
     static void swapAlternate(int[] arr, int n){
-        //Check odd or even no using bitwise operator
-        if((n & 1) == 0){
-            for(int i = 0; i < n; i+=2){
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-            }
-        } else {
-            for(int i = 0; i < n - 1; i += 2){
+        //using efficient way
+        for(int i = 0; i < n; i+=2){
+            if((i+1) < n){
                 int temp = arr[i];
                 arr[i] = arr[i+1];
                 arr[i+1] = temp;
@@ -28,7 +22,7 @@ public class SwapAlternate {
 
     public static void main(String[] args){
 
-        int[] arr = {1,2,3,4,5,6};
+        int[] arr = {1,2,3,4,5};
         int n = arr.length;
 
         printArray(arr);
