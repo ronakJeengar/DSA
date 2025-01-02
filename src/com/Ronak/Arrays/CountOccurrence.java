f50505d9e396f1d8be06740c590107c92878f347
Ronak Jeengar <ronakjeengar7860@gmail.com>
2025-01-02 22:17:37 +0530

    count occurrence problem added

---
+++ src/com/Ronak/Arrays/CountOccurrence.java
@@ -0,0 +1,40 @@
+package com.ronak.arrays;
+
+import java.util.HashMap;
+
+public class CountOccurrence {
+
+    public static void main(String[] args) {
+        int[] array = {5, 2, 8, 2, 3, 2, 1, 3, 2};
+        int target = 2;
+
+        int count = countOccurrences(array, target);
+        System.out.println("Element " + target + " occurs " + count + " times.");
+
+        HashMap<Integer, Integer> occurrences = countAllNumberOccurrences(array);
+        System.out.println("Occurrences: " + occurrences);
+    }
+
+    public static int countOccurrences(int[] arr, int target) {
+        int count = 0;
+
+        for (int num : arr) {
+            if (num == target) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static HashMap<Integer, Integer> countAllNumberOccurrences(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        return map;
+    }
+
+}
