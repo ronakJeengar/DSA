6a8c1fc5b2296aae5edcfb279f22a2f9a4a93d6f
Ronak Jeengar <ronakjeengar7860@gmail.com>
2025-01-02 21:56:34 +0530

    Method name changed

--- src/com/Ronak/Arrays/FindArraySorted.java
+++ src/com/Ronak/Arrays/FindArraySorted.java
@@ -4,11 +4,11 @@ public class FindArraySorted {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 2, 4, 5};
-        boolean ans = findArraySortedBruteForce(arr);
+        boolean ans = findArraySorted(arr);
         System.out.println(ans);
     }
 
-    static boolean findArraySortedBruteForce(int[] arr) {
+    static boolean findArraySorted(int[] arr) {
 
         for (int i = 0; i < arr.length - 1; i++) {
            if(arr[i] > arr[i + 1]){
