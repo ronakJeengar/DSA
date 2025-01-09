e854005a28937b97235213d13117811a0d09dce3
Ronak Jeengar <ronakjeengar7860@gmail.com>
2025-01-09 23:29:13 +0530

    find the 0 sum subarray and find maximum product of subarray problem added

---
+++ src/com/Ronak/Arrays/FindAllSubArrays.java
@@ -0,0 +1,56 @@
+package com.ronak.arrays;
+
+public class FindAllSubArrays {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, -3, 4, 5};
+        findAllSubArrays(arr);
+        boolean ans = findSpecificSumSubArray(arr);
+        System.out.println("ans is : " + ans);
+        findMaximumProductSubArray(arr);
+    }
+
+    static void findAllSubArrays(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int currentSum = 0;
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    static boolean findSpecificSumSubArray(int[] arr) {
+        int target = 9;
+        for (int i = 0; i < arr.length; i++) {
+            int currentSum = 0;
+            for (int j = i; j < arr.length; j++) {
+                currentSum += arr[j];
+                System.out.print(arr[j] + " ");
+
+                if (currentSum == target) {
+                    System.out.println("Sub Array with sum " + target + " found.");
+                    return true;
+                }
+            }
+            System.out.println();
+        }
+        return false;
+    }
+
+    static void findMaximumProductSubArray(int[] arr) {
+
+        int maxProduct = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            int currentProduct = 1;
+            for (int j = i; j < arr.length; j++) {
+                currentProduct *= arr[j];
+                maxProduct = Math.max(currentProduct, maxProduct);
+            }
+        }
+        System.out.println("Maximum Product Subarray is: " + maxProduct);
+    }
+}
