67099f6b3abe7e7fd1ece758878cba693792a33c
Ronak Jeengar <ronakjeengar7860@gmail.com>
2025-02-11 22:22:15 +0530

    print all sub arrays in list added

--- src/com/Ronak/Arrays/FindAllSubArrays.java
+++ src/com/Ronak/Arrays/FindAllSubArrays.java
@@ -1,13 +1,16 @@
 package com.ronak.arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FindAllSubArrays {
 
     public static void main(String[] args) {
 
         int[] arr = {1, 2, -3, 4, 5};
-        findAllSubArrays(arr);
+//        findAllSubArrays(arr);
+//        printAllSubArrays(arr);
         boolean ans = findSpecificSumSubArray(arr);
         System.out.println("ans is : " + ans);
         findMaximumProductSubArray(arr);
@@ -16,11 +19,21 @@ public class FindAllSubArrays {
     static void findAllSubArrays(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
-
                 for (int k = i; k <= j; k++) {
                     System.out.print(arr[k] + " ");
                 }
                 System.out.println();
+            }
+        }
+    }
+
+    public static void printAllSubArrays(int[] arr) {
+        int n = arr.length;
+        for (int start = 0; start < n; start++) {
+            List<Integer> subArray = new ArrayList<>();
+            for (int end = start; end < n; end++) {
+                subArray.add(arr[end]);
+                System.out.println(subArray);
             }
         }
     }
