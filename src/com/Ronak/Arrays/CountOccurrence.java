a75a4e8077c8e098842bf3a5ef2a89a4ead48f84
Ronak Jeengar <ronakjeengar7860@gmail.com>
2025-02-11 22:20:58 +0530

    count occurrence of target added

--- src/com/Ronak/Arrays/CountOccurrence.java
+++ src/com/Ronak/Arrays/CountOccurrence.java
@@ -13,6 +13,9 @@ public class CountOccurrence {
 
         HashMap<Integer, Integer> occurrences = countAllNumberOccurrences(array);
         System.out.println("Occurrences: " + occurrences);
+
+        int count1 = countSingleOccurrence(array, target);
+        System.out.println("Element " + target + " occurs " + count1 + " times.");
     }
 
     public static int countOccurrences(int[] arr, int target) {
@@ -37,4 +40,12 @@ public class CountOccurrence {
         return map;
     }
 
+    public static int countSingleOccurrence(int[] arr, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        return map.get(target);
+    }
 }
